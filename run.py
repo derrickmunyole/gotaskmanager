daf66ae69bd7bb2da3bb52cdb8674a52ea1445ca
derrickmunyole <derrickjust@outlook.com>
2024-07-13 20:55:24 +0300

    Added additional modules to the shell context

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 from app import create_app, db
 from app.user.model import User
+from app.task.model import Task
+from app.project.model import Project
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, }
+    return {
+        'db': db,
+        'User': User,
+        'Task': Task,
+        'project': Project
+    }
 
 
 if __name__ == '__main__':
