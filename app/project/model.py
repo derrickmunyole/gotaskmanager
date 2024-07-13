be6ff957293d3099213701479c0bc78c88c541cf
derrickmunyole <derrickjust@outlook.com>
2024-07-13 20:48:22 +0300

    Updated description to be nullable

--- app/project/model.py
+++ app/project/model.py
@@ -4,5 +4,5 @@ from app import db
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text)
     tasks = db.relationship('Task', back_populates='project', lazy=True)
