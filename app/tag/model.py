648bed2d293c6834b9446b92255838137a718d52
derrickmunyole <derrickjust@outlook.com>
2024-07-17 19:43:13 +0300

    Set tags id field to autoincrement

--- app/tag/model.py
+++ app/tag/model.py
@@ -2,6 +2,6 @@ from app import db
 
 
 class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     tasks = db.relationship('Task', secondary='task_tags_m2m', back_populates='tags')
