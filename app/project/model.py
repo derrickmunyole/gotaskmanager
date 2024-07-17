ff3a1ab3a235cf2b705f0b9d41da1c2616f23bee
derrickmunyole <derrickjust@outlook.com>
2024-07-17 14:43:14 +0300

    Added autoincrement property for project id

--- app/project/model.py
+++ app/project/model.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.Text)
     tasks = db.relationship('Task', back_populates='project', lazy=True)
