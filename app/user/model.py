3fda98def20eb317f465c990a0bcf3410c1c93e3
derrickmunyole <derrickjust@outlook.com>
2024-07-17 22:44:55 +0300

    Added set_password to the User model

--- app/user/model.py
+++ app/user/model.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 
@@ -13,14 +13,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.Text)
     tasks = db.relationship('Task', back_populates="assignee", lazy='dynamic')
 
-    def __init__(self, username, email, password_hash):
-        self.username = username
-        self.email = email
-        self.password_hash = password_hash
-
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
