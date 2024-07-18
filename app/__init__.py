01d0fa6a82289a1cde470b2e447917aa1609db16
derrickmunyole <derrickjust@outlook.com>
2024-07-18 10:29:35 +0300

    Removed unused login manager and made create_app leaner

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
-login = LoginManager()
-login.login_view = 'main.login'
-
-
-@login.user_loader
-def load_user(user_id):
-    from .user.model import User
-    return User.query.get(int(user_id))
 
 
 def create_app(config_class=Config):
@@ -24,27 +15,22 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
-    login.init_app(app)
 
     api = Api(app, version='1.0', title='Task Management API', description='An API for a task manager application')
 
-    from app.user import model as user_model
-    from app.task import model as task_model
-    from app.project import model as project_model
-    from app.tag import model as tag_model
-    from app.task_tags import model as task_tag_model
-    from app.subtask import model as subtask_model
-    from app.comment import model as comment_model
-
+    # Import and register namespaces
     from app.user.routes import ns as user_ns
     from app.task.routes import ns as task_ns
     from app.project.routes import project_ns
     from app.comment.routes import comments_ns
     from app.tag.routes import tags_ns
+
     api.add_namespace(user_ns, path='/user')
     api.add_namespace(task_ns, path='/task')
     api.add_namespace(project_ns, path='/project')
     api.add_namespace(comments_ns, path='/comment')
     api.add_namespace(tags_ns, path='/tag')
+
+    from app import models
 
     return app
