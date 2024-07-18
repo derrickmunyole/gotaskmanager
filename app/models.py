3475518504d2f4ceead97d6110be024ededfe7e4
derrickmunyole <derrickjust@outlook.com>
2024-07-18 14:59:45 +0300

    Added models file

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app.comment.model import Comment
+from app.project.model import Project
+from app.subtask.model import Subtask
+from app.tag.model import Tag
+from app.task.model import Task
+from app.user.model import User
+
+__all__ = ['User', 'Task', 'Project', 'Tag', 'Subtask', 'Comment']
