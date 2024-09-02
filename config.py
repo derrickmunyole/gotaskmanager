8e00db3bf6e5bf345db4556ed28908697a1112e5
Derrick <derrickjust@outlook.com>
2024-09-02 22:02:16 +0300

    Added vercel ci config and test configuration

--- config.py
+++ config.py
@@ -10,3 +10,9 @@ class Config:
     REFRESH_SECRET_KEY = os.environ.get('REFRESH_SECRET_KEY') or 'super-secret-refresh_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost:5432'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'postgresql://localhost:5432/test_db'
+
