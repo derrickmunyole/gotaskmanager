dba6233b364b869b892e0e1da8279a6d848cea66
Derrick <derrickjust@outlook.com>
2024-09-07 19:03:25 +0300

    Setup sqlite for ci/cd

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     FERNET_KEY = os.environ.get('FERNET_KEY')
     REFRESH_SECRET_KEY = os.environ.get('REFRESH_SECRET_KEY') or 'super-secret-refresh_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost:5432'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///dev.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV=os.environ.get('FLASK_ENV')
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'postgresql://localhost:5432/test_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///test.db'
