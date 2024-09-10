14b73fb55e1adcf52c4acf08e3f4f11c2172caf5
Derrick <derrickjust@outlook.com>
2024-09-10 12:25:13 +0300

    Setup flask to use unix sockets in production mode

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from config import Config
+from werkzeug.serving import run_simple
 
 app = create_app()
 
@@ -13,6 +14,7 @@ def make_shell_context():
 
 if __name__ == '__main__':
     if Config.is_production():
-        app.run(host='0.0.0.0', port=80, debug=False)
+        socket_path = '/var/www/myapp/myflaskapp.sock'
+        run_simple('unix://' + socket_path, None, app)
     else:
         app.run(host='127.0.0.1', port=5000, debug=True)
