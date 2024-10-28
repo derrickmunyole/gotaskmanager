d3f63310c6aab8a69bd10b32190de172264a2b2a
Derrick <derrickjust@outlook.com>
2024-10-28 15:12:32 +0300

    Refactored app architecture

--- app/user/services.py
+++
@@ -1,9 +0,0 @@
-from app.models import RefreshToken
-from app.refreshtoken.services import generate_new_access_token
-
-
-def refresh_access_token(session_id):
-    refresh_token_entry = RefreshToken.query.filter_by(session_id=session_id).first()
-    if refresh_token_entry:
-        return generate_new_access_token(refresh_token_entry.user_id)
-    return None
